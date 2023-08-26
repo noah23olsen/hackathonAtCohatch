1eeb66065271c5a88b26e43c6716bccc8203238d
KVinyl <kevinyilie@gmail.com>
2023-08-26 11:37:23 -0400

    Add submit button

--- my-app/src/components/HomeComponent.vue
+++ my-app/src/components/HomeComponent.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-    
+    <button @click="getGptResponse">{{this.responseFromGPT}}</button>
   </div>
 </template>
 
 <script>
 import OpenAiService from '../services/OpenAiService';
 export default {
-created(){
-  this.getGptResponse()
-},
+  setup() {
+      let topic = "java";  
+      let responseFromGPT=  "";
+      return {topic, responseFromGPT};
+  },
 methods: {
 getGptResponse() {
       OpenAiService.generateChat()
         .then((response) => {
+          this.responseFromGPT = (response.data.choices[0].message.content);
           console.log(response.data.choices[0].message.content);
         })
+        .catch(error=>console.log(error));
     },
 }
 }
--- my-app/src/services/OpenAiService.js
+++ my-app/src/services/OpenAiService.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default {
 generateChat() {
-    const apiKey = process.env.VUE_APP_OPENAI_API_KEY; // Use VUE_APP_ prefix for Vue environment variables
+    const apiKey = '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'; // Use VUE_APP_ prefix for Vue environment variables
 
    return axios.post(
         "https://api.openai.com/v1/chat/completions",
         {
           model: "gpt-3.5-turbo", 
-          messages: [{ role: "user", content: "please interpret this code for me, limit the response to 1000 characters"}],
+          messages: [{ role: "user", content: "please interpret this code for me, limit the response to 500 characters"}],
           temperature: 0.7,
         },
         {
